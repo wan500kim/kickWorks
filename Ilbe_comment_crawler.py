3f7e692a48409f8c7be1eaa19ad6304a72f1195a
adap8709 <55938423+adap8709@users.noreply.github.com>
2022-12-04 18:11:39 +0900

    Update Ilbe_comment_crawler.py

--- Ilbe_comment_crawler.py
+++ Ilbe_comment_crawler.py
@@ -3,17 +3,16 @@ import time
 import pandas as pd
 from selenium.webdriver.common.by import By
 
-#hate_speech_list = pd.read_csv('hate_speech_raw_data.csv')['hate_speech'].tolist()
-hate_speech_list = []
-url_list = pd.read_csv('url_list_v2.csv')['url'].tolist()
+comments = []
+url_list = pd.read_csv('ilbe_url_list.csv')['url'].tolist()
 
 i = 1185
-driver = webdriver.Chrome(executable_path = 'C:/Users/pych/Desktop/VSC_python/chromedriver.exe')
+driver = webdriver.Chrome(executable_path = 'chromedriver.exe')
 for url_num in range(i, len(url_list)):
     driver.get(url_list[url_num])
     comment_list = driver.find_elements(By.XPATH, '//span[contains(@class, \'comment-box\')]')
     for comment in comment_list:
-        hate_speech_list.append(comment.text)
+        comments.append(comment.text)
         print(url_num, comment.text) 
-    pd.Series(hate_speech_list).to_frame(name='hate_speech').drop_duplicates().reset_index(drop=True).to_csv('hate_speech_raw_data.csv', index=False)           
+    pd.Series(comments).to_frame(name='ilbe_comment').drop_duplicates().reset_index(drop=True).to_csv('ilbe_comments.csv', index=False)           
     time.sleep(2)
